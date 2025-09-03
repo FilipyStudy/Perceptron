12f14d663a63d93e4e61f64362708552ffd5ad54
Filipy <filipydasilvacorporativo@gmail.com>
2025-09-02 21:57:26 -0300

    remake: remaking the project

--- Perceptron.py
+++ Perceptron.py
@@ -1,33 +1,40 @@
-import pandas as pd
+import pandas
 import numpy as np
-from pandas import DataFrame
 
-rng = np.random.default_rng()
 
 #Return -1, 0 or 1 using the sign() mathematical function
-def binary_classification(df):
-    #Variables definition
-    len_X = df.shape[1]
-    weights = np.ndarray((df.shape[1], 1), dtype=np.float64)
-    y_predicted = np.ndarray((df.shape[1], 1), dtype=np.float64)
-    np.append(weights, np.random.uniform(-1.0, 1.0, (1, len_X)))
-    iterator = df.iterrows()
+def binary_classification(features, expected_var, epochs):
+    try:
+        #Learning rate variable:
+        r = 0.1
 
-    #Iterate over each row
-    for index, row in iterator:
-        y_predicted = np.multiply(row[index], weights)
-        if np.sum(y_predicted) >= 0:
-            weights = np.subtract(weights, row)
-        elif np.sum(y_predicted) < 0:
-            weights = np.sum(weights, row)
-    return weights
+        #Inicialize random generator.
+        rng = np.random.default_rng()
+
+        #Inicializing the weights array with random values.
+        weights = rng.uniform(low = 0.5, high = 0.5, size = features.size[1])
+
+        #Transform the dataframe in a array of vectors.
+        x_data = features.to_numpy()
+        #Do the iteration in the entire data in the epochs numbers of time.
+        for epoch in range(epochs):
+
+            #Iterate over the items, and update the weights vector if necessary.
+            for x, y in zip(x_data, expected_var):
+
+                #Insert bias, calculate the final output value.
+                x = np.insert(x, 0, 0.5)
+                weights = np.insert(weights, 0, 0.5)
+                output = np.sign(np.dot(x, weights))
+                output = 0 if output <= 0 else 1
+
+                #Correct the weights if it's not correct.
+                if y - output != 0:
+                    first_part = np.multiply(x, (r * (y - output)))
+                    weights = np.add(weights, first_part)
+
+        return weights
 
 
-class SingleLayer:
-    def __init__(self, single_data):
-        self.single_data = single_data
-
-
-class Multilayer:
-    def __init__(self, multi_data):
-            self.multi_data = multi_data
+    except Exception as e:
+        raise e
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import pandas as pd
 
 df = pd.read_csv('dataset/Raisin_Dataset.csv')
 df['Class'] = df['Class'].apply(lambda x: 1 if x == 'Kecimen' else 0)
+df_col = df['Class']
+df = df.drop(columns=['Class'])
+
 if __name__ == "__main__":
     print(df.head())
     print(df.shape)
-    weights = binary_classification(df)
-    print(len(weights))
+    weights = binary_classification(df, df_col, 1)
+    #print(weights)
